40cf57c0e695e2d6b6b7091ba9c6814ca3cee620
r0hin <me@r0h.in>
2020-12-12 13:00:47 -0500

    Criteria D & Updating Feedback

--- y10pp/flutter1/lib/main.dart
+++ y10pp/flutter1/lib/main.dart
@@ -1,92 +1,20 @@
 import 'package:flutter/material.dart';
-// import 'get.dart';
 
-void main() => runApp(new MyApp());
+//Custom widgets
+import 'JobsListView.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'second',
-      theme: new ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: MyScaffold(),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 64),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        // <Widget> is the type of items in the list.
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: FlatButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                splashColor: Colors.blueAccent,
-                onPressed: () {
-                  /*...*/
-                },
-                child: Text(
-                  "Click if Finlaser Bad",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Reddit One',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List of programming jobs'),
+        ),
+        body: Center(child: JobsListView()),
       ),
     );
   }
