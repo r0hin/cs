54ed7f3aa7fed97509531a0464c1b52d179bff92
Rohin <git@r0h.in>
2022-11-08 15:07:33 -0500

    Cave Ting

--- game/src/GameMain.java
+++ game/src/GameMain.java
@@ -1,12 +1,17 @@
 import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.print.attribute.standard.PagesPerMinuteColor;
 import javax.swing.JButton;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class GameMain {
+public class GameMain extends Object {
 	
 	//Build the GUI Components
 	private JFrame frame;
@@ -16,6 +21,9 @@ public class GameMain {
 	private JButton attackBTN;
 	private JButton magicBTN;
 	private JButton defendBTN;
+	
+	//Data Component
+	private Map map;
 
 	private JPanel canvas = new JPanel() {
 		
@@ -23,16 +31,84 @@ public class GameMain {
 		
 			super.paintComponent(g);
 			g.setColor(Color.RED);
-			g.fillRect(0,0,300,300);
-		
+			
+			/*
+			 * 
+			 * This is a better approach now since the color selection
+			 * is contained within the Map class, which from a design 
+			 * perspective is much better. 
+			 * 
+			 */
+			for (int y = 0; y < map.getY(); y = y + 1) {
+				
+				for (int x = 0; x < map.getX(); x = x + 1) {
+					
+					Color c = map.getColor(x,y);
+					g.setColor(c);
+					g.fillRect(x*10,y*10,x*10 + 10,y * 10 + 10);
+				}
+			}
+			
+			
+			
 		
 		}	
 		
 	};
 	
+	private ActionListener action = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			//System.out.println("ACTION");
+			//WHO CLICKED?
+			//e.getSource - gets the source of who was clicked
+			//System.out.println(e.getSource() == attackBTN);
+		}
+		
+		
+	};
+	
+	private KeyListener kl = new KeyListener() {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println(e.getKeyCode());
+			int keyCode = e.getKeyCode();
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Pressed");
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Released");
+		}
+		
+		
+		
+	};
+	
+	
+	
 	
 	public GameMain() {
+		System.out.println("GAME_MAIN_1");
+		System.out.println("MISKEW_MOD");
 		
+		//Construct Data Elements
+		map = new Map(30,30);
+		System.out.println(map);
+				
+				
+				
 		//Construct Graphic Elements
 		frame = new JFrame();
 		panel = new JPanel();
@@ -52,19 +128,31 @@ public class GameMain {
 		
 		//setBounds(x,y,width,heigh);
 		attackBTN.setBounds(5,5,100,30);
+		attackBTN.addActionListener(action);
+		
 		magicBTN.setBounds(5,35,100,30);
+		magicBTN.addActionListener(action);
+		
 		defendBTN.setBounds(5,65,100,30);
+		defendBTN.addActionListener(action);
+		
 		canvas.setBounds(105,5,300,300);
 		
+		//Add the objects/widgets to the screen
 		panel.add(attackBTN);
 		panel.add(magicBTN);
 		panel.add(defendBTN);
 		panel.add(canvas);
 		
 		
+		panel.setFocusable(true);
+	    panel.requestFocus();
+	 	panel.addKeyListener(kl);
+	 	
 		frame.add(panel); //This adds the panel to the frame. 
 		frame.setResizable(false);
 		frame.pack();
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
--- game/src/Map.java
+++ game/src/Map.java
@@ -1,24 +1,49 @@
+import java.awt.Color;
 
 public class Map extends Object{
 
 	private int[][] map;
 	private int range;
+	private Color[] colors;
 	
 	public Map(int x, int y) {
 		
-		range = 3;
+		
+		
+		colors = new Color[2];
+		colors[0] = Color.BLACK;
+		colors[1] = Color.GRAY;
+				
 		map = new int[y][x];
 		
-		generateMap(); 
+		generateCaveSystem(); 
 		
 	}
 	
+	public int getY() { return map.length; }
+	public int getX() { 
+		
+		try {
+			
+		
+			return map[0].length; 
+		
+		} catch (Exception e) {
+			
+			return -1;
+		
+		}
+	}
+	public Color getColor(int x, int y) { return colors[map[y][x]]; }
+	
+	public int getValue(int x, int y) { return map[y][x]; } 
 	/**
 	 * 
 	 * @param n
 	 * @return
 	 */
 	public static int[] generate1DList(int n, int r) {
+		
 		
 		int[] arr = new int[n];
 
@@ -29,31 +54,153 @@ public class Map extends Object{
 		
 		return arr;
 	}
+	
+	/**
+	 * This method generates a randomized cave system by
+	 * randomly selecting a location in the 2D map array
+	 * and then creating a path through it of valid elements
+	 * and setting the position to "empty space".  It does
+	 * this a number of times creating randomized cave system.
+	 * 
+	 * Often this cave system is fully connected, however, this 
+	 * is not always the case.  An interesting next step would be
+	 * to think about how we guarantee a fully connected cave system.
+	 * 
+	 * Think about how you can set up parameters to allow
+	 * the user to pass arguments that help give more 
+	 * flexibility in the design. 
+	 */
+	public void generateCaveSystem() {
+		
+		//This stores our possible directions as an array
+		//in this example we can only go up down, left or
+		//right.  But if diagonal was an option you woudl
+		//add that to the dir array and then it minimizes
+		//other updates. 
+		int[][] dir = 	{	{-1,0},
+						 	{0,1},
+						 	{1,0},
+						 	{0,-1}
+						};
+		
+		//This is the number of independent tunnels we want
+		//the program to generate.  The program will
+		//pick numTunnels random starting points and apply
+		//the algorithm. 
+		int numTunnels = 10;
+		
+		//We are going to populate array with all 1, which
+		//indicates solid rock - we are applying a standard
+		//nested loop structure to traverse a 2D array.
+		for (int y = 0; y < map.length; y = y + 1) {
+			
+			for (int x = 0; x < map[y].length; x = x + 1) {
+			
+				map[y][x] = 1;
+				
+				
+			}
+		}
+
+		//Next we run the tunnel generation component
+		//numTunnels times. The higher the number the more
+		//independent tunnels that are generated
+		for (int i = 0; i < numTunnels; i = i + 1) {
+			
+			//Generate a random starting point
+			//Option: We could add some logic here to 
+			//ensure the starting point is always solid rock
+			//-1
+			int ry = (int)(Math.random()*map.length);
+			int rx = (int)(Math.random()*map[0].length);
+			
+			//stores the real "head" position of the tunnel being 
+			//generated
+			int[] position = {ry,rx};
+			
+			//We can modify the length of the tunnel here 
+			//Option: Make this random each time.
+			int tunnelLength = 100;
+			
+			//We are going to clear tunnel Length bricks. It means
+			//we are going to take 100 steps through the array and 
+			//set the element to 0 which means there is no rock 
+			//anymore
+			for (int j = 0; j < tunnelLength; j = j + 1) {
+				
+				//We need to check if the next position is valid
+				//as a starting place we will assume it is not. 
+				boolean valid = false; //assume invalid
+				
+				//This loop ensures that we find a valid next 
+				//position
+				while (valid == false) {
+					//Generate random direction
+					int r = (int)(Math.random()*dir.length);
+					
+					//generate a next possible location in the 
+					//map array to move. 
+					int ny = ry + dir[r][0];
+					int nx = rx + dir[r][1];
+					
+					//check if position is valid
+					if (nx >= 0 && nx < map.length) {
+						if (ny >= 0 && ny < map[0].length) {
+							//if it is valid we 
+							valid = true; //set valid to true to exit the while loop
+							ry = ny; //set new real head position y
+							rx = nx; //set new real head position x
+							map[rx][ry] = 0; //turn the new head position into a tunnel
+						}
+					}
+				}
+			}
+		
+		
+		}
+		
+		
+		
+	}
 	public void generateMap() {
 	
 		//Approach 1:
-//		for (int x = 0; x < map.length; x = x + 1) {
-//			
-//			for (int y = 0; y < map[x].length; y = y + 1) {
-//				
-//				int r = (int)(Math.random()*range);
-//				map[x][y] = r;
-//				
-//			}
-//		}
-//		
+		for (int x = 0; x < map.length; x = x + 1) {
+			
+			for (int y = 0; y < map[x].length; y = y + 1) {
+				
+				int r = (int)(Math.random()*100);
+				
+				if (r < 65) {
+					map[x][y] = 0;
+				}
+				else if (r < 90) {
+					map[x][y] = 1;
+						
+				}
+				else {
+					map[x][y] = 2;
+					
+				}
+				
+			}
+	}
+		
 		
 		//Approach 2: 
 		
-		for (int x = 0; x < map.length; x = x + 1) {
-
-			map[x] = generate1DList(map[x].length, range);
-
-		}
+		//for (int x = 0; x < map.length; x = x + 1) {
+		
+//			map[x] = generate1DList(map[x].length, range);
+			
+//		}
+		
+		
 	
 		
 		
 	}
+	
 	
 	public String toString() {
 		
@@ -67,27 +214,5 @@ public class Map extends Object{
 			result = result + "\n";
 		}
 		return result;
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//write some test code here:
-		//Construct a new map object
-		Map testMap = new Map(4,3);
-		System.out.println(testMap.toString());
-		
-		
-		//Output the map to console. 
-		
-		
-		
-		
-		
-		
-		
-
-	}
-
+	}	
 }
